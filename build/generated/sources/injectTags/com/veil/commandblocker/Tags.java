7f87e7010bae4dc0ffa17e327d4199bbb738d547
StarlightLily <starlightlily742@gmail.com>
2025-08-03 14:26:21 -0400

    Initial Commit

---
+++ build/generated/sources/injectTags/com/veil/commandblocker/Tags.java
@@ -0,0 +1,9 @@
+package com.veil.commandblocker;
+
+/** Auto-generated tags from RetroFuturaGradle */
+public class Tags {
+    private Tags() {}
+
+    /** Auto-generated tag from RetroFuturaGradle */
+    public static final String VERSION = "5a2c7fe";
+}
